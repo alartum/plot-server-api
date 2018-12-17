c6cbf4309d9d121086fbf1e709d221c67f62b453
Aleksandr Artemenkov <alartum@gmail.com>
2018-12-17 22:30:33 +0300

    Working client

--- client.py
+++ client.py
@@ -4,11 +4,12 @@ from cryptography.fernet import Fernet
 import json
 
 class PlotAPI():
-    def __init__(self, username, key, api_url):
+    def __init__(self, username, key, api_url, verbose=False):
         self.username = username
         self.key = key 
         self.api_url = api_url
         self.encoder = Fernet(key)
+        self.verbose = verbose
 
     def upload(self, entry, data):
         joined = self.username + data
@@ -18,7 +19,8 @@ class PlotAPI():
             "raw" : raw.decode('utf-8')
         }
         res = requests.post(self.api_url + entry, data=json.dumps(body))
-        print(res.text)
+        if self.verbose:
+            print(res.text)
 
 class Project():
     def __init__(self, name, plot_api, fresh_start=False):
@@ -29,33 +31,49 @@ class Project():
         self.files = []
         self.frames = {}
         self.upload_entry = "/api/upload"
+        self.prepare_entry = "/api/prepare"
+        self.prepared = False
+
+    def prepare_project(self):
+        mode = "/append"
+        if not self.cleaned:
+            mode = "/refresh"
+            self.cleaned = True
+        raw = json.dumps([self.name, self.files])
+        res = self.plot_api.upload(self.prepare_entry+mode, raw)
+        self.prepared = True
 
     def add_files(self, files):
+        if self.prepared:
+            print("Project: Can't add any files to the project that is already prepared.")
+            return
         for file in files:
             self.files.append(file)
 
     def add_frame(self, file, step, value):
+        if not self.prepared:
+            print("Project: Project is not prepared. Run prepare_project() first.")
+            return
         if file not in self.files:
-            print('Error: File {} is not detected in project {}.'.format(file, self.name)) 
+            print('Project: File {} is not detected in project {}.'.format(file, self.name))
+            return
         self.frames[file] = [step, value]
 
     def send_frames(self):
-        mode = "/a"
-        if not self.cleaned:
-            mode = "/w"
-            self.cleaned = True
         raw = json.dumps([self.name, self.frames])
-        self.plot_api.upload(self.upload_entry+mode, raw)
+        self.plot_api.upload(self.upload_entry, raw)
         self.frames = {}
 
 key = b'2gS8MlgPhs-jknxmKJi9wBasWYvXjE6lXjGCnWMobns='
 api_url = Config.API_ADDRESS
 
 api = PlotAPI("alartum", key, api_url)
-project = Project("test", api, fresh_start=True)
+project = Project("project1", api, fresh_start=True)
 
-project.add_files(["xs", "ys"])
+project.add_files(["data1", "data2", "bad_file"])
+project.prepare_project()
+
 for i in range(3):
-    project.add_frame("xs", i, i**2)
-    project.add_frame("ys", i, i**0.5)
+    project.add_frame("data1", i, i**2)
+    project.add_frame("data2", i, i**0.5)
     project.send_frames()
